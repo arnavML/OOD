4bd9e631d30bed0e1f0690f2a46c9169e76d7117
Your Name <you@example.com>
2026-05-11 20:36:26 +0200

    full rework controller and view p1

--- Sem3/Controller/Controller.java
+++ Sem3/Controller/Controller.java
@@ -1,5 +1,6 @@
 package Sem3.Controller;
 
+import Sem3.Model.DTOs.CustomerDTO;
 import Sem3.Model.Domain.Bike;
 import Sem3.Model.Domain.Customer;
 import Sem3.Model.Domain.RepairOrder;
@@ -7,6 +8,7 @@ import Sem3.Model.Domain.RepairTask;
 import Sem3.Model.Logic.CustomerRegistry;
 import Sem3.Model.Logic.RepairOrderRegistry;
 import Sem3.View.Printer;
+import Sem3.Model.Mapper.*;
 
 public class Controller { //The Controller class is responsible for handling the logic of the application, including searching for customers, creating repair orders, and managing the details of the repair orders. It interacts with the CustomerRegistry to find customers, the RepairOrderRegistry to manage repair orders, and the Printer to print repair order details. It also stores the currently selected customer and repair order to facilitate getting their details without having to search for them again.
 
@@ -43,95 +45,18 @@ public class Controller { //The Controller class is responsible for handling the
     }
 
     //Method For searching for a customer by their phone number, which will be used in the View when the receptionist/technician searches for a customer
-    public Customer search(int number) {
+    public CustomerDTO search(int number) {
         //Finds the customer in the registry by their phone number and returns the customer object if found, otherwise returns null, and also sets the currentCustomer variable to the found customer so that we can use it later without having to search for it again
         currentCustomer =  customerRegistry.findCustomerByNumber(number);
-        return currentCustomer;
+        CustomerDTO customerDTO = CustomerMapper.toDTO(currentCustomer);
+        return customerDTO;
     }
 
-    //Methods for getting the details of the current customer their bike, and the repair info, which will be used in the View to display the customer's details after searching for them
-    //Methods have been writen in the classes to return the data in a better way but didnt use this time.
-    //Customer details
-    public String customerName() { // Returns the name of the current customer, which will be used in the View to display the customer's name after searching for them
-        return currentCustomer.getName();
-    }
-    public String customerEmail() { // Returns the email of the current customer, which will be used in the View to display the customer's email after searching for them
-        return currentCustomer.getEmail();
-    }
-
-    //Bike details
-    public String bikeBrand() { // Returns the brand of the current customer's bike, which will be used in the View to display the bike's brand after searching for the customer
-        return currentCustomer.getBike().getBrand();
-    }
-    public String bikeModel() { // Returns the model of the current customer's bike, which will be used in the View to display the bike's model after searching for the customer
-        return currentCustomer.getBike().getModel();
-    }
-    public String bikeSerialNumber() { // Returns the serial number of the current customer's bike, which will be used in the View to display the bike's serial number after searching for the customer
-        return currentCustomer.getBike().getSerialNumber();
-    }
-
-    //Repair details
-    public String repairDescription() { // Returns the description of the current repair order, which will be used in the View to display the repair order's description after creating a repair order for the customer
-        return repairOrder.getDescription();
-    }
-    public String repairStatus() { // Returns the status of the current repair order, which will be used in the View to display the repair order's status after creating a repair order for the customer
-        return repairOrder.getStatus();
-    }
-    public String repairDate() { // Returns the date of the current repair order, which will be used in the View to display the repair order's date after creating a repair order for the customer
-        return repairOrder.getDate();
-    }
-    public String repairDiagnosticReport() { // Returns the diagnostic report of the current repair order, which will be used in the View to display the repair order's diagnostic report after performing diagnostics on the customer's bike
-        return repairOrder.getDiagnosticReport();
-    }
-
-
-
-    public void makeRepairOrder(String description, String date, String status) { // logic to create a new repair order for the current customer with the given description, date, and status, and add it to the repair order registry. It also sets the current repair order variable to the newly created repair order so that we can use it later without having to search for it again
-        RepairOrder newOrder = new RepairOrder(currentCustomer,currentCustomer.getCustomerNumber(), description, date, status);
-        repairOrderRegistry.addRepairOrder(newOrder);
-    }
-
-    public RepairOrder findRepairOrdersForCustomer() { // logic to find repair orders for the given customer and return them. It also sets the current repair order variable to the found repair order so that we can use it later without having to search for it again
-        // Implement logic to find repair orders for the given customer
-        repairOrder = repairOrderRegistry.findRepairOrderCustomerByNumber(currentCustomer.getCustomerNumber());
-        return repairOrder;
-    }
-
-    public void addDiagnosticReportToOrder(String diagnosticReport) { // logic to add a diagnostic report to the given repair order
-        // Implement logic to add a diagnostic report to the given repair order
-        repairOrder.setDiagnosticReport(diagnosticReport);
-    }
-
-    public void addRepairTaskToOrder(String taskDescription, Double cost) { // logic to add a repair task to the given repair order
-
-        // Implement logic to add a repair task to the given repair order
-        repairOrder.addRepairTask(taskDescription, cost);
-    }
-    
-    public void printRepairOrderDetails() { // logic to print the details of the given repair order by using the Printer class. It gets the details of the current customer and repair order, formats the repair tasks, and then calls the printer to print all the details.
-        // Implement logic to print the details of the given repair order
-        printer.printRepairOrderDetails(currentCustomer.getCustomerNumber(), repairOrder.getDescription(), repairOrder.getDate(), repairOrder.getStatus(), repairOrder.getDiagnosticReport(), getFormattedRepairTasks());
-    }
-
-    public String getFormattedRepairTasks() { // Helper method to format the repair tasks and their costs into a string that can be printed by the Printer class. It iterates through the list of repair tasks in the current repair order, formats each task's description and cost, and calculates the total cost of all tasks. It then returns a string containing the details of each task and the total cost.
-
-        StringBuilder taskDetails = new StringBuilder();
-        double totalCost = 0.0;
-        
-        // The Controller handles the Model objects (RepairTask) and the math
-        for (RepairTask task : repairOrder.getRepairTasks()) {
-            taskDetails.append("Task: ").append(task.getDescription()).append("\n");
-            taskDetails.append("Cost: ").append(task.getCost()).append("\n\n");
-            totalCost += task.getCost();
-        }
-        
-        taskDetails.append("Total Cost: ").append(totalCost);
-        
-        return taskDetails.toString();
-    }
-
-    public void updateRepairOrderStatus(String status) { // Implement logic to update the status of the given repair order
-        repairOrder.setStatus(status);
+    public void createRepairOrder(int number, String description, String date, String status) { //Creates a new repair order for the given customer with the provided description and date. It also sets the current repair order variable to the newly created repair order so that we can use it later without having to search for it again.
+        Customer customer = customerRegistry.findCustomerByNumber(number);
+        RepairOrder newRepairOrder = new RepairOrder(customer, number, description, date, status);
+        repairOrderRegistry.addRepairOrder(newRepairOrder);
+        this.repairOrder = newRepairOrder;
     }
 
 }
--- Sem3/View/Viewer.java
+++ Sem3/View/Viewer.java
@@ -1,6 +1,8 @@
 package Sem3.View;
 
 import Sem3.Controller.Controller;
+import Sem3.Model.DTOs.BikeDTO;
+import Sem3.Model.DTOs.CustomerDTO;
 
 public class Viewer { //The View class is responsible for interacting with the user and displaying information. It has a reference to the Controller, which it uses to perform actions and retrieve data to display.
 
@@ -19,106 +21,29 @@ public class Viewer { //The View class is responsible for interacting with the u
         basicFlow();
     }
     private void basicFlow() { //Runs the sections of the basic flow
-        receiveBikeFromCustomer();
-        performDiagnosticsAndRepair();
-        checkoutCustomer();
+        System.out.println("Running basic flow...");
+        CustomerDTO customer = controller.search(numberToSearch);
+        System.out.println("Returned customer details from search method:");
+
+        System.out.println("Customer search Details:");
+        printCustomerDetails(customer);
+        printBikeDetails(customer.getBike());
+
+        System.out.println("Details are correct, proceeding to create repair order...");
+
+        controller.createRepairOrder(customer, repairDescription, date, "pending");
     }
 
-    private void receiveBikeFromCustomer() { // Implements the logic for receiving a bike from a customer, which includes searching for the customer by their phone number, displaying their details, and creating a repair order if the details are correct.
-        search(numberToSearch);
-        System.out.println("Showing details for customer to Repair Shop Technician...");
-        showCustomerDetails();
-        showBikeDetails();
-        if (correctDetails()){
-            System.out.println("Details are correct, making repair order..." + "\n");
-            makeRepairOrder(repairDescription, date, "Newly Created");
-        }
-        
+    private void printCustomerDetails(CustomerDTO customer) { //Prints the details of the customer, including their name, customer number, email, and bike details. This is used in the View to display the customer's information after searching for them.
+        System.out.println("---------------------------------------------------------------------------------" + "\n" + "Customer Details:");
+        System.out.println("Name: " + customer.getName());
+        System.out.println("Customer Number: " + customer.getCustomerNumber()); //perhaps redundant since it's the same as the phone number, but it could be useful if we want to change the way we identify customers in the future
+        System.out.println("Email: " + customer.getEmail());
     }
-
-    private void performDiagnosticsAndRepair() { // Implements the logic for performing diagnostics and repair on the customer's bike
-        showRepairOrdersForCustomer();
-        addDiagnosticReportToOrder("The bike needs a new wheel, brake pads, and battery.");
-        addRepairTaskToOrder("new Wheel", 100.0);
-        addRepairTaskToOrder("new Brake Pads", 150.0);
-        addRepairTaskToOrder("new Battery", 40.0);
-        controller.updateRepairOrderStatus("Ready for approval");
-    }
-
-    private void checkoutCustomer() { //Code for checking out the customer, which includes showing the diagnostic report and repair tasks to the customer, and then printing the details of the repair order for the customer to see before they leave the shop
-        showDiagnosticReportForOrder();
-        controller.updateRepairOrderStatus("Accepted");
-        // just have the printer print the details of the repair order, including the diagnostic report and the repair tasks with their costs
-        System.out.println("Printing Repair Order Details..." + "\n");
-        controller.printRepairOrderDetails();
-    }
-
-    private void search(int number) { // Searches for a customer by their phone number
-        System.out.println("Searching for number: " + number);
-        System.out.println();
-        controller.search(number); // This will set the currentCustomer in the controller, which we can then use to get details back to the view without having to search for it again
-    }
-
-    private void showCustomerDetails() { // Displays the details of the current customer, which includes their name and email. It gets this information from the controller.
-        System.out.println("Customer Details:");
-        System.out.println("Customer Name: " + controller.customerName());
-        System.out.println("Customer Email: " + controller.customerEmail());
-        System.out.println();
-    }
-    private void showBikeDetails() { // Displays the details of the current customer's bike, which includes the bike's brand, model, and serial number. It gets this information from the controller.
-        System.out.println("Bike Details:");
-        System.out.println("Bike Brand: " + controller.bikeBrand());
-        System.out.println("Bike Model: " + controller.bikeModel());
-        System.out.println("Bike Serial Number: " + controller.bikeSerialNumber());
-        System.out.println();
-    }
-    private void showRepairOrderDetails() { // Displays the details of the current repair order, which includes its description, status, and date. It gets this information from the controller.
-        System.out.println("Repair Order Details:");
-        System.out.println("Description: " + controller.repairDescription());
-        System.out.println("Status: " + controller.repairStatus());
-        System.out.println("Date: " + controller.repairDate());
-        System.out.println();
-
-    }
-
-    private boolean correctDetails() { //Checks if the details of the customer and bike are correct. Just placeholder.
-        System.out.println("Checking if the details are correct...");
-        System.out.println();
-        return true; // Placeholder return value
-    }
-
-    private void makeRepairOrder (String description, String date, String status) { //Makes repair order via controller
-        controller.makeRepairOrder(description, date, status);
-    }
-
-    private void showRepairOrdersForCustomer() { // Implement logic to show repair orders for the given customer to Technician
-        controller.findRepairOrdersForCustomer();// This will set the current repair order in the controller, which we can then use to get details back to the view without having to search for it again
-        System.out.println("Showing details for customer to Repair Shop Technician...");
-        showCustomerDetails();
-        showBikeDetails();
-        showRepairOrderDetails();
-    }
-
-    private void addDiagnosticReportToOrder(String diagnosticReport) { //Adds a diagnostic report to the current repair order via the controller
-        controller.addDiagnosticReportToOrder(diagnosticReport);
-    }
-    private void addRepairTaskToOrder(String taskDescription, Double cost) { //Adds a repair task to the current repair order via the controller
-        controller.addRepairTaskToOrder(taskDescription, cost);
-    }
-
-    private void showRepairTasksForOrder() { //Shows the repair tasks for the current repair order to the customer. It gets the formatted repair tasks from the controller and prints them to the console.
-        System.out.println("Repair Tasks for Repair Order:");
-        
-        // The View just prints the String provided by the Controller
-        System.out.println(controller.getFormattedRepairTasks());
-        
-        System.out.println();
-    }
-
-    private void showDiagnosticReportForOrder() { //Shows the diagnostic report for the current repair order to the customer. It gets the diagnostic report from the controller and prints it to the console.
-        System.out.println("Showing diagnostic report for repair order to Customer...");
-        System.out.println("Diagnostic Report for Repair Order: " + controller.repairDiagnosticReport());
-        System.out.println();
-        showRepairTasksForOrder();
+    private void printBikeDetails(BikeDTO bike) { //Prints the details of the customer's bike, including the make, model, serial number, and owner's phone number. This is used in the View to display the bike's information after searching for the customer.
+        System.out.println("---------------------------------------------------------------------------------" + "\n" + "Bike Details:");
+        System.out.println("Bike Make: " + bike.getBrand());
+        System.out.println("Bike Model: " + bike.getModel());
+        System.out.println("Bike Serial Number: " + bike.getSerialNumber());
     }
 }
